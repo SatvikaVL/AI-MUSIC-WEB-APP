a64fc56e456e9135e5495f663e653d199fce9143
SatvikaVL <100283639+SatvikaVL@users.noreply.github.com>
2022-12-05 18:52:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
 function preload(){
     song1 = loadSound("song1.mp3");
     song2 = loadSound("song2.mp3");
@@ -10,10 +14,26 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
 function draw(){
     image(video , 0 , 0 , 500 , 500);
 }
 function play(){
     song1.play();
+}
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+        console.log('left wrist x = '+leftwristX+"; left wrist y = "+leftwristY+'; right wrist x = '+rightwristX+"; right wrist y = "+rightwristY);
+
+    }
 }
